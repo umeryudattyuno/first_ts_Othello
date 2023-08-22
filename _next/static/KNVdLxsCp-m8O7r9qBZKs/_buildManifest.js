a20cdbbb246f63c07fc7ff0c83018fd05e732fa3
umeryudattyuno <umeryudattyuno@users.noreply.github.com>
2023-08-22 10:22:55 +0000

    deploy: cbecaa9da2bad56013d9db6b3e8dff190ecc5dbb

---
+++ _next/static/KNVdLxsCp-m8O7r9qBZKs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a3d1503c392946df.css","static/chunks/pages/index-e561e6861109999b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
